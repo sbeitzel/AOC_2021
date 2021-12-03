8e746d066b58ccd89027b3e92cbe3630cc3f91cf
Stephen Beitzel <sbeitzel@pobox.com>
2021-12-03 11:00:04 -0800

    Initial Commit

---
+++ AdventOfCode21/AdventOfCode21App.swift
@@ -0,0 +1,17 @@
+//
+//  AdventOfCode21App.swift
+//  AdventOfCode21
+//
+//  Created by Stephen Beitzel on 12/3/21.
+//
+
+import SwiftUI
+
+@main
+struct AdventOfCode21App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
